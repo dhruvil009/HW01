bbfe0dfb9ef2ffb91f4d48b3255460512177e197
Dhruvil <dshah4@ncsu.edu>
2020-08-14 00:33:29 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='octavia',
+    version='0.1',
+    description='Initial Setup for HW01',
+    author=['Dhruvil', 'Sreemoyee', 'Poorvaja', 'Neeraj', 'Sameer'],
+    author_email='dshah4@ncsu.edu',
+    packages=['octavia'],
+    license='Apache',
+    install_requires=[],
+    url='http://www.github.com',
+    )
